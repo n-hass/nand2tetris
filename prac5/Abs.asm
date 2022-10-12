43d91284a6f0f2abde5d170508e1418dd43fb3f2
n-hass <nickhassan01@icloud.com>
2022-10-12 20:21:24 +1030

    Abs fix (add neg test)

--- prac5/Abs.asm
+++ prac5/Abs.asm
@@ -1,12 +1,23 @@
 // Calculates the absolute value of R1 and stores the result in R0.
 @R1
-D = !M
-D = D+1
+D = M
+@INVERT
+D;JLT
 @R0
 M = D
-
 @END
 0;JMP
+
+
+(INVERT)
+  @R1
+  D = !M
+  D = D+1
+  @R0
+  M = D
+
+  @END
+  0;JMP
 
 (END)
   @END
