642de0ba7e1c2a361b987004df8911e4d6d6dddc
n-hass <nickhassan01@icloud.com>
2022-10-22 00:57:36 +1030

    push/pop

--- prac6/VMTranslator/Lines.hpp
+++ prac6/VMTranslator/Lines.hpp
@@ -8,10 +8,14 @@ struct File {
     __a += in + "\n";
   }
   inline void ins(const std::string &in, const std::string &comment) {
-    __a += in +"\t// "+ comment +"\n";
+    // __a += in +"\t// "+ comment +"\n";
+    __a += in +"\n";
   }
 
   inline std::string str() {
+    if (__a.back() == '\n') {
+      __a.pop_back();
+    }
     return __a;
   }
 };
